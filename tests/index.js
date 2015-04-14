5f84ab73cfc3ed42762f6f0412db299665976722
sudheesh001 <sudheesh1995@outlook.com>
2015-04-14 13:45:08 +0530

    Getting Travis-CI to work instead of fallback

---
+++ tests/index.js
@@ -0,0 +1,12 @@
+(function () {
+
+	var chai = require('chai');
+	var assert = chai.assert;
+
+	var sprint = require('../server/db/sprint.js');
+	var dbIndex = require('../server/routes/index.js')
+	describe ('Sprint Model', function (done) {
+		
+	})
+
+})();
